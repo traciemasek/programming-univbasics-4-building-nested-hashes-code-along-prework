a5d92b5abd5c27eff7d26de78e7f2ae7282bb1c1
Tracie Masek <traciemasek@gmail.com>
2019-06-20 12:00:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -18,7 +18,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {name: "Steven",
+        age: "30",
+        attitude: "confused"},
+        {name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"}
       ]
    }
 }
